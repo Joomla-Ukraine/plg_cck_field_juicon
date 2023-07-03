ab44a77fe635eacafffc14812c93f2e073e7bfb6
Denys <denys@joomla-ua.org>
2023-07-03 04:39:13 +0300

    Fixes

--- tmpl/edit.php
+++ tmpl/edit.php
@@ -1,30 +1,101 @@
 <?php
+/**
+ * UIkit Icon for SEBLOD
+ *
+ * @package          Joomla.Site
+ * @subpackage       plg_cck_field_typo_jumultithumbs
+ *
+ * @author           Denys Nosov, denys@joomla-ua.org
+ * @copyright        2016-2022 (C) Joomla! Ukraine, https://joomla-ua.org. All rights reserved.
+ * @license          GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
-defined( '_JEXEC' ) or die;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
+defined('_JEXEC') or die;
+
+$options2 = JCckDev::fromJSON($this->item->options2);
+
+$icons = [];
+if(@$options2[ 'sprite' ])
+{
+	if(is_file(JPATH_SITE . '/' . @$options2[ 'sprite' ]))
+	{
+		$sprite = @file_get_contents(JPATH_SITE . '/' . @$options2[ 'sprite' ]);
+		$dom    = new DOMDocument;
+		$dom->loadXML($sprite);
+		$symbols = $dom->getElementsByTagName('symbol');
+
+		$items = [];
+		foreach($symbols as $symbol)
+		{
+			$items[] = $symbol->getAttribute('id');
+		}
+
+		$icons = [
+			'storage_field' => 'location',
+			'options'       => implode('||', $items),
+			'required'      => 'required'
+		];
+	}
+}
+else
+{
+	$app = Factory::getApplication();
+	$app->enqueueMessage(Text::_('COM_CCK_ADD_SPRITE_PATH'), 'notice');
+}
+
+// Init
 JCckDev::forceStorage();
 
-// Set
-$displayData	=	array(
-						'config'=>$config,
-						'form'=>array(
-							array(
-								'fields'=>array(
-									JCckDev::renderForm( 'core_icons', $this->item->location, $config, array( 'storage_field'=>'location' ) ),
-									JCckDev::renderBlank()
-								)
-							),
-							array(
-								'fields'=>array(
-									JCckDev::getForm( 'core_storage', $this->item->storage, $config )
-								),
-								'mode'=>'storage'
-							)
-						),
-						'html'=>'',
-						'item'=>$this->item,
-						'script'=>''
-					);
+$sprite_icons = '';
+$sprite_size  = '';
+$sprite_class = '';
+if(@$options2[ 'sprite' ])
+{
+	$sprite_icons = JCckDev::renderForm('core_icons', $this->item->location, $config, $icons);
+	$sprite_size  = JCckDev::renderForm('core_dev_text', @$options2[ 'size' ], $config, [
+		'label'         => 'Size',
+		'defaultvalue'  => '24',
+		'size'          => 50,
+		'storage_field' => 'json[options2][size]'
+	]);
+	$sprite_class = JCckDev::renderForm('core_dev_text', @$options2[ 'class' ], $config, [
+		'label'         => 'Class',
+		'size'          => 50,
+		'storage_field' => 'json[options2][class]'
+	]);
+}
 
-echo JCckDev::renderLayoutFile( 'cck'.JCck::v().'.construction.cck_field.edit', $displayData );
-?>
+// Set
+$displayData = [
+	'config' => $config,
+	'form'   => [
+		[
+			'fields' => [
+				JCckDev::renderForm('core_dev_text', @$options2[ 'sprite' ], $config, [
+					'label'         => 'Sprite Path',
+					'required'      => 'required',
+					'size'          => 50,
+					'storage_field' => 'json[options2][sprite]'
+				]),
+				$sprite_icons,
+				$sprite_size,
+				$sprite_class
+			]
+		],
+		[
+			'fields' => [
+				JCckDev::getForm('core_storage', $this->item->storage, $config)
+			],
+			'mode'   => 'storage'
+		]
+	],
+	'help'   => [],
+	'html'   => '',
+	'item'   => $this->item,
+	'script' => ''
+];
+
+echo JCckDev::renderLayoutFile('cck' . JCck::v() . '.construction.cck_field.edit', $displayData);
--- uikiticon.php
+++ uikiticon.php
@@ -1,125 +1,156 @@
 <?php
+/**
+ * UIkit Icon for SEBLOD
+ *
+ * @package          Joomla.Site
+ * @subpackage       plg_cck_field_typo_jumultithumbs
+ *
+ * @author           Denys Nosov, denys@joomla-ua.org
+ * @copyright        2016-2022 (C) Joomla! Ukraine, https://joomla-ua.org. All rights reserved.
+ * @license          GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
-defined( '_JEXEC' ) or die;
+use Joomla\CMS\Layout\FileLayout;
+
+defined('_JEXEC') or die;
 
 // Plugin
-class plgCCK_FieldUIKITIcon extends JCckPluginField
+class plgCCK_FieldUIkitIcon extends JCckPluginField
 {
-	protected static $type		=	'uikiticon';
+	protected static $type = 'uikiticon';
 	protected static $path;
 
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Construct
+
 	// onCCK_FieldConstruct
-	public function onCCK_FieldConstruct( $type, &$data = array() )
+	public function onCCK_FieldConstruct($type, &$data = [])
 	{
-		if ( self::$type != $type ) {
+		if(self::$type != $type)
+		{
 			return;
 		}
-		parent::g_onCCK_FieldConstruct( $data );
+		parent::g_onCCK_FieldConstruct($data);
 	}
 
 	// onCCK_FieldConstruct_SearchSearch
-	public static function onCCK_FieldConstruct_SearchSearch( &$field, $style, $data = array(), &$config = array() )
+	public static function onCCK_FieldConstruct_SearchSearch(&$field, $style, $data = [], &$config = [])
 	{
-		$data['live']		=	null;
-		$data['match_mode']	=	null;
-		$data['validation']	=	null;
-		$data['variation']	=	null;
+		$data[ 'live' ]       = null;
+		$data[ 'match_mode' ] = null;
+		$data[ 'validation' ] = null;
+		$data[ 'variation' ]  = null;
 
-		parent::onCCK_FieldConstruct_SearchSearch( $field, $style, $data, $config );
+		parent::onCCK_FieldConstruct_SearchSearch($field, $style, $data, $config);
 	}
 
 	// onCCK_FieldConstruct_TypeForm
-	public static function onCCK_FieldConstruct_TypeForm( &$field, $style, $data = array(), &$config = array() )
+	public static function onCCK_FieldConstruct_TypeForm(&$field, $style, $data = [], &$config = [])
 	{
-		$data['live']		=	null;
-		$data['validation']	=	null;
-		$data['variation']	=	null;
-		
-		parent::onCCK_FieldConstruct_TypeForm( $field, $style, $data, $config );
+		$data[ 'live' ]       = null;
+		$data[ 'validation' ] = null;
+		$data[ 'variation' ]  = null;
+
+		parent::onCCK_FieldConstruct_TypeForm($field, $style, $data, $config);
 	}
-	
+
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+
 	// onCCK_FieldPrepareContent
-	public function onCCK_FieldPrepareContent( &$field, $value = '', &$config = array() )
+	public function onCCK_FieldPrepareContent(&$field, $value = '', &$config = [])
 	{
-		if ( self::$type != $field->type ) {
+		if(self::$type != $field->type)
+		{
 			return;
 		}
-		parent::g_onCCK_FieldPrepareContent( $field, $config );
-		
+		parent::g_onCCK_FieldPrepareContent($field, $config);
+
 		// Init
-		$html			=	'';
-		$value			=	$field->location;
-		$html			=	'<span class="uikiticon-'.$value.'"></span>';
-		
-		// Set
-		$field->text		=	$html;
-		$field->typo_target	=	'text';
-		$field->value		=	$value;
-	}
-	
-	// onCCK_FieldPrepareForm
-	public function onCCK_FieldPrepareForm( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
-	{
-		if ( self::$type != $field->type ) {
-			return;
-		}
-		self::$path	=	parent::g_getPath( self::$type.'/' );
-		parent::g_onCCK_FieldPrepareForm( $field, $config );
-		
-		// Init
-		if ( count( $inherit ) ) {
-			$id		=	( isset( $inherit['id'] ) && $inherit['id'] != '' ) ? $inherit['id'] : $field->name;
-		} else {
-			$id		=	$field->name;
-		}
-		
-		// Prepare
-		$value			=	$field->location;
-		$form			=	'<span class="uikiticon-'.$value.'"></span>';
+		$value = $field->location;
+
+		$options2 = JCckDev::fromJSON($field->options2);
+		$html     = (new FileLayout('icon', JPATH_SITE . '/plugins/cck_field/uikiticon/tmpl'))->render([
+			'sprite' => $options2[ 'sprite' ],
+			'icon'   => $value,
+			'size'   => $options2[ 'size' ],
+			'class'  => ($options2[ 'class' ] ? : '')
+		]);
 
 		// Set
-		$field->form	=	$form;
-		$field->value	=	$value;
-		
-		// Return
-		if ( $return === true ) {
-			return $field;
-		}
+		$field->text        = $html;
+		$field->typo_target = 'text';
+		$field->value       = $value;
 	}
-	
-	// onCCK_FieldPrepareSearch
-	public function onCCK_FieldPrepareSearch( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+
+	// onCCK_FieldPrepareForm
+	public function onCCK_FieldPrepareForm(&$field, $value = '', &$config = [], $inherit = [], $return = false)
 	{
-		if ( self::$type != $field->type ) {
+		if(self::$type != $field->type)
+		{
 			return;
 		}
-		
+		self::$path = parent::g_getPath(self::$type . '/');
+		parent::g_onCCK_FieldPrepareForm($field, $config);
+
 		// Prepare
-		self::onCCK_FieldPrepareForm( $field, $value, $config, $inherit, $return );
+		$value = $field->location;
+
+		$options2 = JCckDev::fromJSON($field->options2);
+		$form     = (new FileLayout('icon', JPATH_SITE . '/plugins/cck_field/uikiticon/tmpl'))->render([
+			'sprite' => $options2[ 'sprite' ],
+			'icon'   => $value,
+			'size'   => $options2[ 'size' ],
+			'class'  => ($options2[ 'class' ] ? : '')
+		]);
 		
+		// Set
+		$field->form  = $form;
+		$field->value = $value;
+
 		// Return
-		if ( $return === true ) {
+		if($return === true)
+		{
 			return $field;
 		}
 	}
-	
-	// onCCK_FieldPrepareStore
-	public function onCCK_FieldPrepareStore( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+
+	// onCCK_FieldPrepareSearch
+	public function onCCK_FieldPrepareSearch(&$field, $value = '', &$config = [], $inherit = [], $return = false)
 	{
-		if ( self::$type != $field->type ) {
+		if(self::$type != $field->type)
+		{
+			return;
+		}
+
+		// Prepare
+		self::onCCK_FieldPrepareForm($field, $value, $config, $inherit, $return);
+
+		// Return
+		if($return === true)
+		{
+			return $field;
+		}
+	}
+
+	// onCCK_FieldPrepareStore
+	public function onCCK_FieldPrepareStore(&$field, $value = '', &$config = [], $inherit = [], $return = false)
+	{
+		if(self::$type != $field->type)
+		{
 			return;
 		}
 	}
+
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Render
 
 	// onCCK_FieldRenderContent
-	public static function onCCK_FieldRenderContent( $field, &$config = array() )
+	public static function onCCK_FieldRenderContent($field, &$config = [])
 	{
-		return parent::g_onCCK_FieldRenderContent( $field, 'text' );
+		return parent::g_onCCK_FieldRenderContent($field, 'text');
 	}
-	
+
 	// onCCK_FieldRenderForm
-	public static function onCCK_FieldRenderForm( $field, &$config = array() )
+	public static function onCCK_FieldRenderForm($field, &$config = [])
 	{
-		return parent::g_onCCK_FieldRenderForm( $field );
+		return parent::g_onCCK_FieldRenderForm($field);
 	}
 }
